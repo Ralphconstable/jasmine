5fad83c8d3e28d8c02adf6420d9fbf2186e25ead
Ralphconstable <108046715+Ralphconstable@users.noreply.github.com>
2025-01-28 05:15:11 -0500

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -128,7 +128,7 @@ class Gallery {
         icon.appendChild(h4)
         h4.innerHTML=title
         icon.style.border="1px solid black"
-        icon.style.maxHeight="15vh"
+        icon.style.maxHeight="20%"
 
         return icon
     }
